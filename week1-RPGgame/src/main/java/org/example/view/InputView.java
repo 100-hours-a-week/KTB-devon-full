40d21ccc905067cfa233706a4916e56fb2a9f3ad
WooSH <imdlsrks.mc@gmail.com>
2025-09-14 07:03:25 +0900

    feat: input 추가

--- week1-RPGgame/src/main/java/org/example/view/InputView.java
+++ week1-RPGgame/src/main/java/org/example/view/InputView.java
@@ -1,7 +1,15 @@
 package org.example.view;
 
+import java.util.Scanner;
+
 public class InputView {
+    private Scanner scanner;
 
+    public InputView() {
+        this.scanner = new Scanner(System.in);
+    }
 
-
+    public int getUserInput() {
+        return scanner.nextInt();
+    }
 }
