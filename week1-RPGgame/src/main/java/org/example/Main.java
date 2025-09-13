f573b98984fe85dc050eb84dc9d77ff45c3d98f8
WooSH <imdlsrks.mc@gmail.com>
2025-09-13 21:08:28 +0900

    feat: 애플리케이션 진입점 추가

--- week1-RPGgame/src/main/java/org/example/Main.java
+++ week1-RPGgame/src/main/java/org/example/Main.java
@@ -1,9 +1,16 @@
 package org.example;
 
-//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
-// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+import org.example.app.Application;
+
 public class Main {
     public static void main(String[] args) {
 
+        try{
+            Application app = new Application();
+            app.run();
+
+        }catch (Exception e){
+            System.out.println("[Error]" + e);
+        }
     }
 }
---
+++ week1-RPGgame/src/main/java/org/example/app/Application.java
@@ -0,0 +1,9 @@
+package org.example.app;
+
+public class Application {
+
+    public void run(){
+        System.out.println("애플리케이션 실행");
+
+    }
+}
